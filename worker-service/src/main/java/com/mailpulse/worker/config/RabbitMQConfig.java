ae635bd4c008ed7a62512e494c60a73290bf1624
Mohammed Nour Moussa <moussanour578@gmail.com>
2026-08-15 01:11:50 +0000

    fix : configuration of JsonMessageConverter

--- worker-service/src/main/java/com/mailpulse/worker/config/RabbitMQConfig.java
+++ worker-service/src/main/java/com/mailpulse/worker/config/RabbitMQConfig.java
@@ -1,5 +1,12 @@
 package com.mailpulse.worker.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +20,50 @@ public class RabbitMQConfig {
     public static final String EMAIL_ROUTING_KEY = "notifications.email";
 
     public static final String DLX_EXCHANGE = "notifications.dlx";
+    public static final String EMAIL_DLQ = "notifications.email.dlq";
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public DirectExchange notificationsExchange() {
+        return new DirectExchange(NOTIFICATIONS_EXCHANGE);
+    }
+
+    @Bean
+    public DirectExchange notificationsDlx() {
+        return new DirectExchange(DLX_EXCHANGE);
+    }
+
+    @Bean
+    public Queue emailQueue() {
+        return QueueBuilder.durable(EMAIL_QUEUE)
+                .withArgument("x-dead-letter-exchange", DLX_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", EMAIL_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Queue emailDlq() {
+        return QueueBuilder.durable(EMAIL_DLQ).build();
+    }
+
+    @Bean
+    public Binding emailBinding() {
+        return BindingBuilder.bind(emailQueue()).to(notificationsExchange()).with(EMAIL_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding emailDlqBinding() {
+        return BindingBuilder.bind(emailDlq()).to(notificationsDlx()).with(EMAIL_ROUTING_KEY);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
